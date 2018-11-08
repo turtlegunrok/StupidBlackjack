6a4e7723712fa5e2570186c861264282ebca823a
Caleb Jobe <cjobe@fenwaygroup.com>
2018-11-07 20:16:03 -0600

    changed locations and added number check

--- Implementation/StupidBlackjackSln/FrmNewGame.Designer.cs
+++ Implementation/StupidBlackjackSln/FrmNewGame.Designer.cs
@@ -43,7 +43,7 @@
             // picPlayerCard1
             // 
             this.picPlayerCard1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.picPlayerCard1.Location = new System.Drawing.Point(116, 1);
+            this.picPlayerCard1.Location = new System.Drawing.Point(60, 1);
             this.picPlayerCard1.Name = "picPlayerCard1";
             this.picPlayerCard1.Size = new System.Drawing.Size(106, 144);
             this.picPlayerCard1.TabIndex = 0;
@@ -52,7 +52,7 @@
             // picPlayerCard2
             // 
             this.picPlayerCard2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.picPlayerCard2.Location = new System.Drawing.Point(253, 1);
+            this.picPlayerCard2.Location = new System.Drawing.Point(210, 1);
             this.picPlayerCard2.Name = "picPlayerCard2";
             this.picPlayerCard2.Size = new System.Drawing.Size(106, 144);
             this.picPlayerCard2.TabIndex = 1;
@@ -61,16 +61,17 @@
             // picPlayerCard3
             // 
             this.picPlayerCard3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.picPlayerCard3.Location = new System.Drawing.Point(386, 1);
+            this.picPlayerCard3.Location = new System.Drawing.Point(360, 1);
             this.picPlayerCard3.Name = "picPlayerCard3";
             this.picPlayerCard3.Size = new System.Drawing.Size(106, 144);
             this.picPlayerCard3.TabIndex = 2;
             this.picPlayerCard3.TabStop = false;
+            this.picPlayerCard3.Click += new System.EventHandler(this.picPlayerCard3_Click);
             // 
             // picPlayerCard4
             // 
             this.picPlayerCard4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.picPlayerCard4.Location = new System.Drawing.Point(522, 1);
+            this.picPlayerCard4.Location = new System.Drawing.Point(510, 1);
             this.picPlayerCard4.Name = "picPlayerCard4";
             this.picPlayerCard4.Size = new System.Drawing.Size(106, 144);
             this.picPlayerCard4.TabIndex = 3;
@@ -79,7 +80,7 @@
             // picPlayerCard5
             // 
             this.picPlayerCard5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.picPlayerCard5.Location = new System.Drawing.Point(666, 1);
+            this.picPlayerCard5.Location = new System.Drawing.Point(660, 1);
             this.picPlayerCard5.Name = "picPlayerCard5";
             this.picPlayerCard5.Size = new System.Drawing.Size(106, 144);
             this.picPlayerCard5.TabIndex = 4;
@@ -88,7 +89,7 @@
             // btnHit
             // 
             this.btnHit.AutoSize = true;
-            this.btnHit.Location = new System.Drawing.Point(358, 179);
+            this.btnHit.Location = new System.Drawing.Point(343, 175);
             this.btnHit.Name = "btnHit";
             this.btnHit.Size = new System.Drawing.Size(44, 29);
             this.btnHit.TabIndex = 5;
@@ -99,7 +100,7 @@
             // btnStand
             // 
             this.btnStand.AutoSize = true;
-            this.btnStand.Location = new System.Drawing.Point(494, 179);
+            this.btnStand.Location = new System.Drawing.Point(442, 175);
             this.btnStand.Name = "btnStand";
             this.btnStand.Size = new System.Drawing.Size(57, 29);
             this.btnStand.TabIndex = 6;
@@ -110,7 +111,7 @@
             // 
             this.lblPlayerScore.AutoSize = true;
             this.lblPlayerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPlayerScore.Location = new System.Drawing.Point(396, 108);
+            this.lblPlayerScore.Location = new System.Drawing.Point(384, 93);
             this.lblPlayerScore.Name = "lblPlayerScore";
             this.lblPlayerScore.Size = new System.Drawing.Size(106, 37);
             this.lblPlayerScore.TabIndex = 7;
--- Implementation/StupidBlackjackSln/FrmNewGame.cs
+++ Implementation/StupidBlackjackSln/FrmNewGame.cs
@@ -10,96 +10,111 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace StupidBlackjackSln {
+namespace StupidBlackjackSln
+{
 
-    public partial class FrmNewGame : Form {
-    private Deck deck;
-    private Player player1;
-    private PictureBox[] picPlayerCards;
-    public int currentCard;
+    public partial class FrmNewGame : Form
+    {
+        private Deck deck;
+        private Player player1;
+        private PictureBox[] picPlayerCards;
+        public int currentCard;
 
-        public FrmNewGame() {
+        public FrmNewGame()
+        {
             InitializeComponent();
             currentCard = 0;
             timer1.Interval = 10;
             timer1.Tick += new EventHandler(Timer_Tick);
             picPlayerCards = new PictureBox[5];
 
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 5; i++)
+            {
                 picPlayerCards[i] = Controls.Find("picPlayerCard" + (i + 1).ToString(), true)[0] as PictureBox;
             }
 
-                timer1.Start();
+            timer1.Start();
         }
 
         void Timer_Tick(object sender, EventArgs e)
         {
-
-            int x = picPlayerCards[currentCard].Location.X;
-            int y = picPlayerCards[currentCard].Location.Y;
-
-            picPlayerCards[currentCard].Location = new Point(x, y + 10);
-
-            if (y > 215)
+            if (currentCard < 5)
             {
-                currentCard += 1;
-                timer1.Stop();
-            }
-            if(currentCard == 1)
-            {
-                timer1.Start();
+                int x = picPlayerCards[currentCard].Location.X;
+                int y = picPlayerCards[currentCard].Location.Y;
+
+                picPlayerCards[currentCard].Location = new Point(x, y + 10);
+
+                if (y > 215)
+                {
+                    currentCard += 1;
+                    timer1.Stop();
+                }
+                if (currentCard == 1)
+                {
+                    timer1.Start();
+                }
             }
 
         }
-        private void FrmNewGame_Load(object sender, EventArgs e) {
-      deck = new Deck(FindBitmap);
-      player1 = new BlackjackPlayer();
-      
-      player1.giveHand(new List<Card>() { deck.dealCard(), deck.dealCard() });
-      showHand();
-    }
-
-    private void showHand() {
-      for (int i = 0; i < player1.Hand.Count(); i++) {
-        picPlayerCards[i].BackgroundImage = player1.Hand[i].Bitmap;
-      }
-      lblPlayerScore.Text = player1.Score.ToString();
-    }
-
-    private void FrmNewGame_FormClosed(object sender, FormClosedEventArgs e) {
-      foreach (Form f in Application.OpenForms) {
-        f.Close();
-      }
-    }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void FrmNewGame_Load(object sender, EventArgs e)
         {
+            deck = new Deck(FindBitmap);
+            player1 = new BlackjackPlayer();
 
+            player1.giveHand(new List<Card>() { deck.dealCard(), deck.dealCard() });
+            showHand();
         }
-        private void btnHit_Click(object sender, EventArgs e) {
-      player1.giveCard(deck.dealCard());
+
+        private void showHand()
+        {
+            for (int i = 0; i < player1.Hand.Count(); i++)
+            {
+                picPlayerCards[i].BackgroundImage = player1.Hand[i].Bitmap;
+            }
+            lblPlayerScore.Text = player1.Score.ToString();
+        }
+
+        private void FrmNewGame_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            foreach (Form f in Application.OpenForms)
+            {
+                f.Close();
+            }
+        }
+
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            player1.giveCard(deck.dealCard());
             picPlayerCard1.Show();
             timer1.Start();
 
             showHand();
-    }
+        }
 
 
-        private Bitmap FindBitmap(string value, string suit) {
-      string textName = "";
-      int valueAsNum;
-      if (int.TryParse(value, out valueAsNum)) {
-        textName += "_";
-      }
+        private Bitmap FindBitmap(string value, string suit)
+        {
+            string textName = "";
+            int valueAsNum;
+            if (int.TryParse(value, out valueAsNum))
+            {
+                textName += "_";
+            }
 
-      textName += value;
-      textName += "_of_";
-      textName += suit;
+            textName += value;
+            textName += "_of_";
+            textName += suit;
 
-      return (Bitmap)Resources.ResourceManager.GetObject(textName);
-    }
+            return (Bitmap)Resources.ResourceManager.GetObject(textName);
+        }
 
         private void lblPlayerScore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picPlayerCard3_Click(object sender, EventArgs e)
         {
 
         }
